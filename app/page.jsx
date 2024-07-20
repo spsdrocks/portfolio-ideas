e91c0af31640e9e8ff54f1d1ad6d1b6f4681c0c6
spsdrocks <64557557+spsdrocks@users.noreply.github.com>
2024-07-19 22:02:57 -0700

    started title page and slideshow format

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,20 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      
+      <div class = "slide-frame">
+        <TitlePage />
+      </div>
     </>
+  );
+}
+
+
+
+
+function TitlePage() {
+  return (
+    <section class = "slide-wrapper, title-slide">
+      <h1>Test Text 1 - Title Page</h1>
+    </section>
   );
 }
